c5e2199b6ce917bb6e826210300a924dc5fe245b
luizsmatos <gusstavopdm@gmail.com>
2021-11-05 10:22:28 -0300

    feat: Adiciona uma nova Task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,17 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim() !== '') {
+      setTasks([
+        ...tasks,
+        {
+          id: Math.random(),
+          title: newTaskTitle,
+          isComplete: false,
+        },
+      ]);
+      return setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
